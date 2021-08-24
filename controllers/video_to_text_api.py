bcea1c28262911c352fd2587014a40d9403b6fd5
sarat <sara0504157494@gmail.com>
2021-08-24 12:47:44 +0300

    capture-comit

--- controllers/video_to_text_api.py
+++ controllers/video_to_text_api.py
@@ -15,8 +15,7 @@ def open_url_in_chrome(url, mode='headed'):
             
     elif mode == 'headless':   
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        
+        options.add_argument('--headless')    
     options.add_argument("--auto-open-devtools-for-tabs")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
@@ -45,17 +44,17 @@ def accept_T_and_C(driver):
     """
     # Click I agree
     driver.find_element_by_xpath("//*[@id='yDmH0d']/c-wiz/div/div/div/div[2]/div[1]/div[4]/form/div[1]/div/button/div[2]").click()
-    sleep(2)
+    # sleep(2)
     try:
         # click 'no thanks' if it pops up
         driver.find_element_by_xpath("//*[@id='dismiss-button']").click()
     except:
-        sleep(2)
-    
+        # sleep(2)
+        pass
+#פונקצית תמלול  
 def get_transcript(driver, mode):
     
     global count 
-    
     driver.implicitly_wait(10)
     
     if mode=='headed':
@@ -68,7 +67,7 @@ def get_transcript(driver, mode):
         
         # Click 'Open transcript'
         driver.find_element_by_xpath("//*[@id='items']/ytd-menu-service-item-renderer/tp-yt-paper-item").click()
-        sleep(3)
+        # sleep(3)
     
     elif mode=='headless':
         # Click 'More actions'
@@ -76,7 +75,7 @@ def get_transcript(driver, mode):
             driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[8]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/yt-icon-button/button").click()
 
         except:
-            sleep(3)
+            # sleep(3)
             count += 1
             if count < 5:
                 driver.refresh()
@@ -91,7 +90,7 @@ def get_transcript(driver, mode):
             driver.find_element_by_xpath("//*[@id='items']/ytd-menu-service-item-renderer/tp-yt-paper-item").click()
 
         except:
-            sleep(3)
+            # sleep(3)
             count += 1
             if count < 5:
                 driver.refresh()
@@ -105,37 +104,44 @@ def get_transcript(driver, mode):
     print("Copying transcript ")
     transcript_element = driver.find_element_by_xpath("//*[@id='body']/ytd-transcript-body-renderer")
     transcript = transcript_element.text
-
     return transcript
-
+#פונקציה המעצבת את הטקסט
 def transcript2df(transcript):
     if transcript == None:
         return "None in transcript."
     transcript = transcript.split('\n')
     transcript_timestamps = transcript[::2]
-    
     transcript_text = transcript[1::2]
+    dic={}
+    j=0
+    for i in transcript_timestamps:
+        dic[i]=transcript_text[j]
+        j+=1
+    print(dic)
+
     df = pd.DataFrame({'timestamp':transcript_timestamps, 
                    'text':transcript_text})
     
     return df
+#פונקציה שמקבלת ניתוב ומנהלת את כל התמלול
 def GetTranscript(url):
     
     mode = 'headless'
-    
+    #שולח נתיב ומקבל את הדף
     driver = open_url_in_chrome(url, mode)
-    
+    #שולח דף ומקבל תמלול
     transcript = get_transcript(driver, mode)
-    
+    #סוגר את הדף
     driver.close()
-    	
+    #עיצוב הטקסט
     df = transcript2df(transcript)
-    
     print('Saving transcript ')
+    #שמירה לקובץ
     df.to_csv('out_transcript_timestamped.csv', index=False) 
     with open("out_transcript_text_only.txt", "w") as text_file:
         print(" ".join(" ".join(df.text.values).split()), file=text_file)
     print('Done')
+    #החזרת התמלול
     return " ".join(" ".join(df.text.values).split())
 
 @app.route('/Transcrire', methods=['POST'])
